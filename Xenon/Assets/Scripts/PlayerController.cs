3b3ef388db531b7da645a1c24b635b53e4959f6a
Roger Chen <chen.rog@northeastern.edu>
2018-02-13 21:08:40 -0500

    Player stuff.

--- Xenon/Assets/Scripts/PlayerController.cs
+++ Xenon/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,20 @@ public class PlayerController : MonoBehaviour {
 
 	public float run_speed;
     public float jump_speed;
+    public float dash_speed;
 
-    private bool grounded;
+    private int jumps;
+    private int dashes;
     private Rigidbody2D rb;
+
+    private bool facing_right;
 
 	// Use this for initialization
 	void Start () {
-        grounded = false;
+        jumps = 0;
+        dashes = 0;
         rb = GetComponent<Rigidbody2D>();
+        facing_right = true;
     }
 
     private void Update()
@@ -22,10 +28,27 @@ public class PlayerController : MonoBehaviour {
         bool jump = Input.GetKeyDown(KeyCode.Space);
 
         // Check if player is allowed to jump
-        if (jump && grounded)
+        if (jump && jumps > 0)
         {
             rb.AddForce(new Vector2(0, 100) * jump_speed);
-            this.grounded = false;
+            this.jumps -= 1;
+        }
+
+        // Player wants to dash
+        bool dash = Input.GetKeyDown(KeyCode.LeftShift);
+
+        // Check if the player is allowed to dash
+        if (dash && dashes > 0)
+        {
+            // check if we dash left or right
+            var move = new Vector2(100, 0);
+
+            if (!facing_right)
+            {
+                move *= -1;
+            }
+
+            rb.AddForce(move * dash_speed);
         }
     }
 
@@ -38,15 +61,24 @@ public class PlayerController : MonoBehaviour {
         // Create a vector for horizontal movement
         Vector2 horizontalMovement = new Vector2(moveHorizontal, 0) * run_speed;
 
+        // Check if we're still facing right
+        if (moveHorizontal != 0)
+        {
+            facing_right = moveHorizontal > 0 ? true : false;
+        }
+
         // Add movement
-        transform.Translate(horizontalMovement);
+        rb.velocity = new Vector2(moveHorizontal * run_speed, rb.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Ground"))
         {
-            this.grounded = true;
+            this.jumps = 1;
+            this.dashes = 1;
         }
+
+        rb.velocity = new Vector2(0, rb.velocity.y);
     }
 }
