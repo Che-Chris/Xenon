c8f7eecb7cad3dfb5da4b38e0a8d9aeabce78694
Christopher Che <che.c@northeastern.edu>
2018-02-13 20:03:33 -0500

    Working on enemy scripts

--- Xenon/Assets/Scripts/EnemyAI.cs
+++ Xenon/Assets/Scripts/EnemyAI.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	public Transform target = null;
+
+	public int maxRange;
+	public int minRange;
+	public float runSpeed;
+
+	private Vector2 targetTran;
+	private Vector2 direction;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
+		
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (target == null) {
+			return;
+		}
+
+		transform.LookAt (target);
+		float distance = Vector2.Distance (transform.position, target.position);
+		bool tooClose = distance < minRange;
+		Vector2 direction = tooClose ? Vector2.right : Vector2.left;
+
+		transform.Translate (direction);
 	}
 }
