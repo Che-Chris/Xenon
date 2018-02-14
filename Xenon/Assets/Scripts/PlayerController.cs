6f112859f63ef23016a2c5fadee6ec225782cd24
Roger Chen <chen.rog@northeastern.edu>
2018-02-13 19:26:48 -0500

    folders added and player controller updated

--- Xenon/Assets/Scripts/PlayerController.cs
+++ Xenon/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
-	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float horizontal = Input.GetAxis ("Horizontal");
-		rb.AddForce(new Vector2()
-	}
+	void FixedUpdate ()
+    {
+        // Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Create a vector for horizontal movement
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        // Add movement
+        transform.Translate(movement * speed);
+    }
 }
