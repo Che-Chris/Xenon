fbccf8a5c291f2896dd007548e61fa81091eec2e
Roger Chen <chen.rog@northeastern.edu>
2018-02-13 20:03:04 -0500

    Player script is done.

--- Xenon/Assets/Scripts/PlayerController.cs
+++ Xenon/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public float run_speed;
+    public float jump_speed;
+
+    private bool grounded;
+    private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+        grounded = false;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Player wants to jump
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        // Check if player is allowed to jump
+        if (jump && grounded)
+        {
+            rb.AddForce(new Vector2(0, 100) * jump_speed);
+            this.grounded = false;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
     {
         // Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         // Create a vector for horizontal movement
-        Vector2 movement = new Vector2(moveHorizontal, 0);
+        Vector2 horizontalMovement = new Vector2(moveHorizontal, 0) * run_speed;
 
         // Add movement
-        transform.Translate(movement * speed);
+        transform.Translate(horizontalMovement);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            this.grounded = true;
+        }
     }
 }
