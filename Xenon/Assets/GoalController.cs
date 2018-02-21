20c9edaa42b9a17da742e9c6c804628a77f7cdbb
Jackson Bowman <jacksoncrossbowman@gmail.com>
2018-02-20 19:15:45 -0500

    web build 0

---
+++ Xenon/Assets/GoalController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour {
+
+	public SceneLoader sl;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		sl.LoadImmediate ();
+	}
+
+
+}
