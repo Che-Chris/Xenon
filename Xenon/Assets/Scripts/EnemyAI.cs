93884dcf6dc3d76d50c28814e59765dfc32ebe55
Christopher Che <che.c@northeastern.edu>
2018-03-20 20:52:48 -0400

    Stopped AI from falling off with invisible game object colliders

--- Xenon/Assets/Scripts/EnemyAI.cs
+++ Xenon/Assets/Scripts/EnemyAI.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 
-	public Transform target;
 	private Vector2 direction;
+	private int point;
+	private bool chasing;
+	// private float start;
+	// private float end;
 
-	public int maxRange;
-	public int minRange;
+	private Transform target;
+	public Transform[] points;
+	//public Transform end;
 
 	// Use this for initialization
 	void Start () {
-		target = null;
+		point = 1;
+		target = points[point];
+		chasing = false;
 	}
 		
 	
 	// Update is called once per frame
 	void Update () {
-		if (target == null) {
-			return;
-		}
-
 		float distance = Vector2.Distance (transform.position, target.position);
-		bool tooClose = distance < minRange;
 
 		if (transform.position.x < target.position.x) {
 			direction = Vector2.right;
@@ -31,18 +32,27 @@ public class EnemyAI : MonoBehaviour {
 			direction = Vector2.left;
 		}
 
-		transform.Translate (direction * Time.deltaTime);
+		transform.Translate (direction * Time.deltaTime * 2);
+
+		if (!chasing) {
+			if (Mathf.Abs(transform.position.x - target.transform.position.x) < 0.1f) {
+				point = (point + 1) % points.Length;
+				target = points [point];
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "Player") {
 			target = other.transform;
+			chasing = true;
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
 		if (other.tag == "Player") {
-			target = null;
+			target = points[point];
+			chasing = false;
 		}
 	}
 }
