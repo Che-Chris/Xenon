25c975da3268fa9781b0c00c21cc176ddd21aaf4
Roger Chen <chen.rog@northeastern.edu>
2018-04-10 20:30:50 -0400

    Seedz are random

---
+++ Xenon/Assets/Scripts/SeedScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SeedScript : MonoBehaviour
+{
+    public Sprite[] seedz;
+
+    private SpriteRenderer sr;
+    private int seed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        seed = Random.Range(0, 27);
+        sr.sprite = seedz[seed];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
