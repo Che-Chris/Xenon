79b0dec38f726f92ad416421d2cd736101177ba5
Roger <chen.rog@husky.neu.edu>
2018-04-24 01:08:05 -0400

    Dialogue and UI overhaul.

---
+++ Xenon/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+    
+    public Image sorrel;
+    public Image gramma;
+    public Text nameText;
+    public Text dialogue;
+
+    private Queue<string> lines;
+
+	// Use this for initialization
+	void Start () {
+        lines = new Queue<string>();
+        sorrel.enabled = false;
+        gramma.enabled = false;
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FindObjectOfType<DialogueTrigger>().TriggerDialogue();
+        }
+    }
+
+    public void StartDialogue(Dialogue d)
+    {
+        // get name
+        nameText.text = d.name;
+
+        // set correct character to active
+        if (d.name == "SORREL")
+        {
+            sorrel.enabled = true;
+            gramma.enabled = false;
+        }
+        else if (d.name == "GRAMMA")
+        {
+            sorrel.enabled = false;
+            gramma.enabled = true;
+        }
+
+        // print lines
+        lines.Clear();
+        lines.Enqueue(d.line);
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence ()
+    {
+        string line = lines.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(line));
+    }
+
+    IEnumerator TypeSentence (string line)
+    {
+        dialogue.text = "";
+        foreach (char letter in line.ToCharArray())
+        {
+            dialogue.text += letter;
+            yield return null;
+        }
+    }
+}
---
+++ Xenon/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public Dialogue[] dialogue;
+    public Queue<Dialogue> d;
+
+    public void Start()
+    {
+        d = new Queue<Dialogue>();
+
+        foreach (Dialogue line in dialogue)
+        {
+            d.Enqueue(line);
+        }
+
+        TriggerDialogue();
+    }
+
+    public void TriggerDialogue()
+    {
+        if (d.Count == 0)
+        {
+            FindObjectOfType<SceneLoader>().LoadImmediate();
+        }
+
+        else
+        {
+            Dialogue dia = d.Dequeue();
+            FindObjectOfType<DialogueManager>().StartDialogue(dia);
+        }
+    }
+}
